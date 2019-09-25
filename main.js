5ea0677a90a66d22d75cf1fced57169870d7e3e4
Marco Serrano <marcoTheWebDev@gmail.com>
2019-09-25 18:04:07 -0500

    Added fav buttons that trigger an alert.

--- main.js
+++ main.js
@@ -2,3 +2,13 @@ $(document).ready(function()  {
     $('.header').height($(window).height());
     console.log($(window).height());
 })
+
+$(".btn-fav").click(function(){
+    $(".alert").toggleClass("hidden");
+    setTimeout(hideAlert, 1500);
+})
+
+function hideAlert(){
+    
+    $(".alert").addClass("hidden");
+}
